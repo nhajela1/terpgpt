081986ba09bce036a8ace7276527931908cd0ea4
fortune710 <fortune0208@yahoo.com>
2024-08-23 12:55:25 -0400

    pushed data from supabase to pinecone

---
+++ python-backend/database.py
@@ -0,0 +1,13 @@
+import os
+from supabase import create_client, Client
+
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_ANON_KEY")
+
+client: Client = create_client(url, key)
+
+
+def get_reviews(page = 0):
+    max_limit = 1000
+    response = client.table("professor-reviews").select("*").limit(max_limit).offset(page * max_limit).execute()
+    return response.data
--- python-backend/setup_rag.py
+++ python-backend/setup_rag.py
@@ -4,11 +4,17 @@ from pinecone import Pinecone, ServerlessSpec
 from openai import OpenAI
 import os
 import json
+from database import get_reviews
+
+
+
 
 # Initialize Pinecone
 pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 
 # Create a Pinecone index
+'''
+
 pc.create_index(
     name="rag",
     dimension=1536,
@@ -16,37 +22,46 @@ pc.create_index(
     spec=ServerlessSpec(cloud="aws", region="us-east-1"),
 )
 
-# Load the review data
-data = json.load(open("reviews.json"))
-
-processed_data = []
+'''
 client = OpenAI()
-
-# Create embeddings for each review
-for review in data["reviews"]:
-    response = client.embeddings.create(
-        input=review['review'], model="text-embedding-3-small"
-    )
-    embedding = response.data[0].embedding
-    processed_data.append(
-        {
-            "values": embedding,
-            "id": review["professor"],
-            "metadata":{
-                "review": review["review"],
-                "subject": review["subject"],
-                "stars": review["stars"],
-            }
-        }
-    )
-
 # Insert the embeddings into the Pinecone index
 index = pc.Index("rag")
-upsert_response = index.upsert(
-    vectors=processed_data,
-    namespace="ns1",
-)
-print(f"Upserted count: {upsert_response['upserted_count']}")
+
+
+
+for page in range(32):
+    # Load the review data
+    data = get_reviews(page)
+    #data returns { id: int, slug: str, review: str, expected_grade: str } as list
+
+    # Create embeddings for each review
+    for review in data:
+        try:
+            response = client.embeddings.create(
+                input=review['review'], model="text-embedding-3-small"
+            )
+            embedding = response.data[0].embedding
+
+            processed_data = [
+                {
+                    "values": embedding,
+                    "id": str(review["id"]),
+                    "metadata":{
+                        "review": review["review"],
+                        "slug": review["slug"],
+                        "expected_grade": review["expected_grade"],
+                    }
+                }
+            ]
+
+            upsert_response = index.upsert(
+                vectors=processed_data,
+                namespace="ns1",
+            )
+            print(f"Upserted count: {upsert_response['upserted_count']}")
+        except Exception as e:
+            print(str(e))
+
 
 # Print index statistics
 print(index.describe_index_stats())
